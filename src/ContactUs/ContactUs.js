dec4b872c3057fa8b356ca1cf7914c97dc30fa0c
whiffy <bryanzhao168@yahoo.ca>
2023-03-30 22:30:53 -0700

    Complete the form tomorrow.

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Text, Title } from "@mantine/core";
+import { Box, Grid, Text, Title, Flex } from "@mantine/core";
 import React from "react";
 import "./ContactUs.css";
 
@@ -7,7 +7,9 @@ const ContactUs = () => {
     <>
       <Grid>
         <Grid.Col span={6} className="contact-us-grid-form-left">
-          <Title order={2}>Contact Us</Title>
+          <Title order={2} style={{ color: "#0B4C95" }}>
+            Contact Us
+          </Title>
           <Text>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl vel
@@ -21,16 +23,42 @@ const ContactUs = () => {
         </Grid.Col>
         <Grid.Col span={6}></Grid.Col>
       </Grid>
-      <Box className="footer-contact-us">
-        <Title order={2} className="jw-footer-title">
-          JOINT WELL VIETNAM
-        </Title>
-        <Grid>
-          <Grid.Col span={4}>Test</Grid.Col>
-          <Grid.Col span={4}>Test</Grid.Col>
-          <Grid.Col span={4}>Test</Grid.Col>
-        </Grid>
-      </Box>
+
+      <div className="footer-contact-us">
+        <Flex direction="column" className="column-contact-us-footer-flex">
+          <Title order={2} className="jw-footer-title">
+            JOINT WELL VIETNAM
+          </Title>
+          <hr style={{ width: "90%", textAlign: "left", marginTop: "1rem" }} />
+
+          <Grid className="footer-grid-for-contacts">
+            <Grid.Col span={4} style={{ background: "#0B4C95" }}>
+              <Text style={{ maxWidth: "18rem", color: "white" }}>
+                Joint Well Head Office: Room 702, Building No.3, Guo Zhan
+                Fortune Center, No.18 Qin Ling Road, Qingdao China 266061 Tel:
+                +86 532 81118060 81118058 E-mail: angela@jointwellcaps.com
+              </Text>
+            </Grid.Col>
+            <Grid.Col span={4} style={{ background: "#0B4C95" }}>
+              {" "}
+              <Text style={{ maxWidth: "18rem", color: "white" }}>
+                Joint Well Head Office: Room 702, Building No.3, Guo Zhan
+                Fortune Center, No.18 Qin Ling Road, Qingdao China 266061 Tel:
+                +86 532 81118060 81118058 E-mail: angela@jointwellcaps.com
+              </Text>
+            </Grid.Col>
+            <Grid.Col span={4} style={{ background: "#0B4C95" }}>
+              {" "}
+              <Text style={{ maxWidth: "18rem", color: "white" }}>
+                Joint Well Head Office: Room 702, Building No.3, Guo Zhan
+                Fortune Center, No.18 Qin Ling Road, Qingdao China 266061 Tel:
+                +86 532 81118060 81118058 E-mail: angela@jointwellcaps.com
+              </Text>
+            </Grid.Col>
+          </Grid>
+        </Flex>
+      </div>
+      <div style={{ back: "#0B4C95" }}></div>
     </>
   );
 };
