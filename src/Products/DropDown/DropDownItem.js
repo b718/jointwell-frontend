85bbcd1a1523abd7e1be09bc2f3524268bdd3309
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-04-02 16:22:52 -0700

    Filter concept is working but there a few things to fix.

--- src/Products/DropDown/DropDownItem.js
+++ src/Products/DropDown/DropDownItem.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./DropDown.css";
 import { Text } from "@mantine/core";
+import { ProductsContext, SetProductsContext } from "../Products";
 
 const DropDownItem = ({ name }) => {
   const [active, setActive] = useState(false);
+  const newProducts = useContext(SetProductsContext);
+
+  const settingProducts = () => {
+    if (active) {
+      newProducts("test");
+    } else {
+      newProducts("");
+    }
+  };
   return (
     <Text
       fz="sm"
       className={active ? "drop-down-item-each-active" : "drop-down-item-each"}
       style={{ maxWidth: "3.2rem" }}
-      onClick={() => setActive(!active)}
+      onClick={() => {
+        setActive(!active);
+        settingProducts();
+      }}
     >
       {name}
     </Text>
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./Products.css";
-import { Grid, Box, Center } from "@mantine/core";
+import { Grid, Box, Center, Button } from "@mantine/core";
 import DropDown from "./DropDown/DropDown";
 import HatCard from "./HatCard/HatCard";
 
+export const ProductsContext = React.createContext();
+export const SetProductsContext = React.createContext();
+//Recall that with context API we can now avoid prop drilling!
+
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [click, setClick] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     //we need to do async await since its an api fetch
@@ -20,41 +26,56 @@ const Products = () => {
 
   return (
     <>
-      <Grid
-        columns={12}
-        gutter={40}
-        grow={true}
-        style={{ maxWidth: "97vw", marginTop: "3rem", marginLeft: "2rem" }}
-      >
-        <Grid.Col span={2} style={{ maxWidth: "10rem" }}>
-          <DropDown dropDownName={"STYLE"} />
-          <Box style={{ marginTop: "0.2rem", maxWidth: "5rem" }}>
-            {" "}
-            <DropDown dropDownName={"MATERIAL"} />
-          </Box>
-          <Box style={{ marginTop: "0.2rem", maxWidth: "5rem" }}>
-            {" "}
-            <DropDown dropDownName={"DECORATION"} />
-          </Box>
-        </Grid.Col>
+      <ProductsContext.Provider value={products}>
+        <SetProductsContext.Provider value={setSelectedCategory}>
+          <Grid
+            columns={12}
+            gutter={40}
+            grow={true}
+            style={{ maxWidth: "97vw", marginTop: "3rem", marginLeft: "2rem" }}
+          >
+            <Grid.Col span={2} style={{ maxWidth: "10rem" }}>
+              <Box onClick={() => setClick(!click)}>
+                <DropDown dropDownName={"STYLE"} />
+              </Box>
+              <Box style={{ marginTop: "0.2rem", maxWidth: "5rem" }}>
+                {" "}
+                <DropDown dropDownName={"MATERIAL"} />
+              </Box>
+              <Box style={{ marginTop: "0.2rem", maxWidth: "5rem" }}>
+                {" "}
+                <DropDown dropDownName={"DECORATION"} />
+              </Box>
+            </Grid.Col>
 
-        <Grid.Col span={10} style={{ maxWidth: "90vw", marginTop: "1rem" }}>
-          {" "}
-          <Center>
-            <Grid>
-              {products.map((product) => {
-                return (
-                  <HatCard
-                    path={product.path}
-                    id={product.id}
-                    name={product.name}
-                  />
-                );
-              })}
-            </Grid>
-          </Center>
-        </Grid.Col>
-      </Grid>
+            <Grid.Col span={10} style={{ maxWidth: "90vw", marginTop: "1rem" }}>
+              {" "}
+              <Center>
+                <Grid>
+                  {products
+                    .filter((product) => {
+                      console.log(product);
+                      if (!selectedCategory) {
+                        return true;
+                      } else {
+                        return product.style.includes(selectedCategory);
+                      }
+                    })
+                    .map((product) => {
+                      return (
+                        <HatCard
+                          path={product.path}
+                          id={product.id}
+                          name={product.name}
+                        />
+                      );
+                    })}
+                </Grid>
+              </Center>
+            </Grid.Col>
+          </Grid>
+        </SetProductsContext.Provider>
+      </ProductsContext.Provider>
     </>
   );
 };
