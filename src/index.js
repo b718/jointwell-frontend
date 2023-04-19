19e41d23f9d8104a545390d5b41c932c4a305ad7
whiffy <bryanzhao168@yahoo.ca>
2023-04-19 16:46:36 -0700

    Re put the data in

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 //disableReactDevTools();
-
+  
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
