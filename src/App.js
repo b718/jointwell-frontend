2f03f059e9ee58955e29fad3d3a0fe5ac00a9475
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-04-22 17:25:35 -0700

    Added a new pic for the icon in the tab.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage/Homepage";
 import Compliance from "./Compliance/Compliance";
 import React from "react";
-import ContactUs from "./ContactUs/ContactUs";
+// import ContactUs from "./ContactUs/ContactUs";
 import Products from "./Products/Products";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/about-us" element={<Homepage />} />
           <Route path="/compliance" element={<Compliance />} />
-          <Route path="/contact-us" element={<ContactUs />} />
+          {/* <Route path="/contact-us" element={<ContactUs />} /> */}
           <Route path="/products" element={<Products />} />
         </Routes>
       </div>
